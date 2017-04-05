46cb17c61f2ad55eaac5caf1edc090c7583b71c4
maxvey1111 <veymaksim99@gmail.com>
2017-04-05 22:49:28 +0300

    Add files via upload

---
+++ HW7/T2.java
@@ -0,0 +1,19 @@
+package HW7;
+
+	import java.io.BufferedReader;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	public class T2 {
+		public static void main(String[] args) throws IOException {	
+			System.out.println("Enter text:");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String text = br.readLine();
+			String[] t= text.split(" ");
+			for (int i = 0; i < t.length; i++) {
+				if (!t[i].isEmpty()) {
+					String s = t[i]+" ";
+					System.out.print(s);
+				}
+			}					
+		}
+	}
